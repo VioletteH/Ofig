0f7909d76cd524f02aaf4ea73855abdfad80c590
Melissa Cpz <melissa.coupez@oclock.school>
2024-11-13 14:54:11 +0100

    finish him

--- app/controllers/bookmarksController.js
+++ app/controllers/bookmarksController.js
@@ -2,58 +2,67 @@
 import figurineDataMapper from '../figurineDataMapper.js';
 
 const bookmarksController = {
-  
+
   async bookmarksPage(request, response) {
+    // request.session.bookmarks = request.session.bookmarks ?? [];
+
+    if (!request.session.bookmarks) {
+      request.session.bookmarks = [];
+    };
+
     response.render('favoris', { bookmarks: request.session.bookmarks });
   },
 
   async bookmarksAdd(request, response) {
 
-    try{
+    try {
 
       const figurineId = parseInt(request.params.id, 10);
-  
-      if (!request.session.bookmarks) { 
-        request.session.bookmarks = []; 
-      }; 
 
-      const isFigurineInList = request.session.bookmarks.some(figurine => figurine.id === figurineId); 
+      if (!request.session.bookmarks) {
+        request.session.bookmarks = [];
+      };
 
-      if(!isFigurineInList){
-        const figurine = await figurineDataMapper.getOneFigurine(figurineId); 
+      const isFigurineInList = request.session.bookmarks.some(figurine => figurine.id === figurineId);
+
+      if (!isFigurineInList) {
+        const figurine = await figurineDataMapper.getOneFigurine(figurineId);
         request.session.bookmarks.push(figurine);
         console.log(request.session.bookmarks);
       };
 
 
     }
-    catch(error){
+    catch (error) {
       console.error(error);
-      return response.status(500).send('Server Error');    
+      return response.status(500).send('Server Error');
     }
 
     response.redirect('/bookmarks');
   },
 
   async bookmarksDelete(request, response) {
-
-    try{
-
+    
+    try {
       const figurineId = parseInt(request.params.id, 10);
+      
+      if (!request.session.bookmarks) {
+        request.session.bookmarks = [];
+      }
 
-      const isFigurineInList = request.session.bookmarks.some(figurine => figurine.id === figurineId); 
+      const isFigurineInList = request.session.bookmarks.some(figurine => figurine.id === figurineId);
 
-      if(isFigurineInList){
-        const figurine = await figurineDataMapper.getOneFigurine(figurineId); 
-        const figurineToDelete = request.session.bookmarks.filter(figurine);
-        console.log(figurineToDelete);
+      if (isFigurineInList) {
+        const figurine = await figurineDataMapper.getOneFigurine(figurineId);
+        const figurineBookmarkIndex = request.session.bookmarks.indexOf(figurine);
+        request.session.bookmarks.splice(figurineBookmarkIndex, 1);
       };
 
 
     }
-    catch(error){
+    catch (error) {
       console.error(error);
-      return response.status(500).send('Server Error');    
+      return response.status(500).send('Server Error');
     }
 
     response.redirect('/bookmarks');
